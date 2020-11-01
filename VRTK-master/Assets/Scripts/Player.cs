b5719e536a1a1b8cd1d4118e9644502048c0df40
Carldst <44940410+Carldst@users.noreply.github.com>
2020-11-01 18:23:59 +0100

    Some adjustements

--- VRTK-master/Assets/Scripts/Player.cs
+++ VRTK-master/Assets/Scripts/Player.cs
@@ -10,19 +10,28 @@ public class Player : MonoBehaviour
     public float speed = 0;
     public TextMeshProUGUI countText;
     public GameObject endMessage; // all elements are gameObject
-    public TextMeshProUGUI AccText;
+    private int count;
     public Boolean isMobileBuild;   
     void Start()
     {
         count = 0; //initiate count 
         endMessage.SetActive(false); //disable the end message
+        
+        SetCountText();
         if (isMobileBuild)
         {
             // enable readingsensor values
             //InputSystem.EnableDevice(UnityEngine.InputSystem.Accelerometer.current);
         }
     }
-
+    void SetCountText()
+    {
+        countText.text = "Score : " + count.ToString();
+        if (count >= 25)
+        {
+            endMessage.SetActive(true);
+        }
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "PickUp")
